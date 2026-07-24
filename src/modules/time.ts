5c671708f05e801a89c0909df8ed0e20ba7bfa37
gtxpit <gtxpit.jr@gmail.com>
2026-07-25 00:18:42 +0700

    first typescript file

--- src/modules/time.ts
+++ src/modules/time.ts
@@ -1,13 +1,13 @@
-export function initClock() {
+export function initClock(): void {
     function updateLocalTime() {
-        const now = new Date()
+        const now: Date = new Date()
         const timeString = now.toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
-        const timeElement = document.getElementById('cityTime')
+        const timeElement = document.getElementById('cityTime') as HTMLElement
         if (timeElement) {
             timeElement.textContent = timeString
         }
     }
-    
+
     updateLocalTime()
     setInterval(updateLocalTime, 1000)
 }
